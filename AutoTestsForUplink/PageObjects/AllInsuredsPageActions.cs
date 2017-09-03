e0f94b717320791c89e5fbeec5baa62fc8afba38
aumolotok <aumolotok@mail.ru>
2017-09-03 19:16:32 +0300

    AllInsuredpageExtensionMethodAdd

---
+++ AutoTestsForUplink/PageObjects/AllInsuredsPageActions.cs
@@ -0,0 +1,17 @@
+﻿using Autotests.PageOdjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autotests.PageObjects
+{
+    static class AllInsuredsPageActions
+    {
+        static public void GoToCreatingNewInsured(this AllInsuredsPage Page)
+        {
+            Page.AddNewButton.Click();
+        }
+    }
+}
